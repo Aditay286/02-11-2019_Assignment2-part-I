cc4715a7f06982c4ec59fefbfe71c6669d315f38
Aditya <45618714+Aditay286@users.noreply.github.com>
2019-11-06 23:32:56 +0530

    Update Q1.cs

--- Q1.cs
+++ Q1.cs
@@ -6,20 +6,20 @@ namespace _02_11_2019_Assignment2
   {
       public static void Run()
       {
-         string input,input2;int no,k,flag,ans=0;
+         string input,input2;int no,pos,flag,ans=0;
          Console.WriteLine("Enter the no");
         input=Console.ReadLine();
         Console.WriteLine("Enter the position of digit from last u want to extract");  
         input2=Console.ReadLine();   
-        if(Int32.TryParse(input,out no)&&Int32.TryParse(input2,out k))
+        if(Int32.TryParse(input,out no)&&Int32.TryParse(input2,out pos))
         {
-          flag=k;
+          flag=pos;
           while(flag-->0)
           {
             ans=no%10;
             no/=10;
           }
-         Console.WriteLine("For position={0} from last value is {1}",k+1,ans);
+         Console.WriteLine("For position={0} from last value is {1}",pos,ans);
         }
         else
         {
